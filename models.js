fd61ff60d22803ea11027f407d8d5bd3df584c80
mMmenges <63979919+mMmenges@users.noreply.github.com>
2020-07-26 10:24:47 +0200

    make it better

---
+++ models.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose'),
+    bcrypt = require('bcrypt');
+
+let movieSchema = mongoose.Schema({
+    Title: { type: String, required: true },
+    Description: { type: String, required: true },
+    Genre: { Name: String, Description: String },
+    Director: {
+        Name: String,
+        Bio: String,
+        Birth: String
+    },
+    Released: String,
+    Staring: [String],
+    ImagePath: String,
+    Featured: Boolean
+});
+
+let userSchema = mongoose.Schema({
+    Username: { type: String, required: true },
+    Password: { type: String, required: true },
+    Email: { type: String, required: true },
+    Birthday: Date,
+    FavoriteMovies: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Movie' }]
+});
+
+userSchema.statics.hashPassword = (password) => {
+    return bcrypt.hashSync(password, 10);
+};
+
+userSchema.methods.validatePassword = function(password) {
+    console.log(password, this.Password);
+    return bcrypt.compareSync(password, this.Password);
+};
+
+let Movie = mongoose.model('Movie', movieSchema);
+let User = mongoose.model('User', userSchema);
+
+module.exports.Movie = Movie;
+module.exports.User = User;
+
+mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
