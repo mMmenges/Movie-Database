bddcc1703e6c41f5a4dbd1a67f55e27c60b6086f
Michael Menges <mmenges82@gmail.com>
2020-08-01 14:11:07 +0200

    update heroku

--- index.js
+++ index.js
@@ -45,8 +45,8 @@ const { check, validationResult } = require('express-validator');
 
 
 //mongoose.connect('mongodb://localhost:27017/myFlixDB', { useNewUrlParser: true, useUnifiedTopology: true });
+//mongoose.connect(process.env.CONNECTION_URI, { useNewURLParser: true, useUnifiedTopology: true });
 mongoose.connect(process.env.CONNECTION_URI, { useNewURLParser: true, useUnifiedTopology: true });
-//mongoose.connect(process.env.CONNECTION_URI, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
 app.use(morgan('common'));
