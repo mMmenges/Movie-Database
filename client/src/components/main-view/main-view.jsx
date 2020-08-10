29b5ff88a4729c1b3cd37f33047c5e674da5fa64
Michael Menges <mmenges82@gmail.com>
2020-08-10 14:34:54 +0200

    Task 3.3

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -17,7 +17,7 @@ export class MainView extends React.Component {
     // One of the "hooks" available in a React Component
     componentDidMount() {
         axios
-            .get('https://myflix-api.herokuapp.com//movies')
+            .get('https://oldmyflix-api.herokuapp.com/movies')
             .then((response) => {
                 // Assign the result to the state
                 this.setState({
