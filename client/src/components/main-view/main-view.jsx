b1761b4c9be3839b3029620db90c64962b74b480
Michael Menges <mmenges82@gmail.com>
2020-08-10 14:26:35 +0200

    next try

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -1,46 +1,71 @@
 import React from 'react';
 import axios from 'axios';
-import axios from 'axios';
 
 import { MovieCard } from '../movie-card/movie-card';
 import { MovieView } from '../movie-view/movie-view';
 
 export class MainView extends React.Component {
-
     constructor() {
         super();
 
         this.state = {
             movies: null,
-            selectedMovie: null
+            selectedMovie: null,
+            user: null,
         };
     }
-
+    // One of the "hooks" available in a React Component
     componentDidMount() {
-        /* ... */
+        axios
+            .get('https://myflix-api.herokuapp.com//movies')
+            .then((response) => {
+                // Assign the result to the state
+                this.setState({
+                    movies: response.data,
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
+            });
     }
 
     onMovieClick(movie) {
         this.setState({
-            selectedMovie: movie
+            selectedMovie: movie,
         });
     }
 
+    onLoggedIn(user) {
+        this.setState({
+            user,
+        });
+    }
 
     render() {
-        const { movies, selectedMovie } = this.state;
+        const { movies, selectedMovie, user } = this.state;
+        if (!user)
+            return <LoginView onLoggedIn={(user) => this.onLoggedIn(user)} />;
 
         // Before the movies have been loaded
         if (!movies) return <div className="main-view" />;
 
         return (
             <div className="main-view">
-                {selectedMovie
-                    ? <MovieView movie={selectedMovie} />
-                    : movies.map(movie => (
-                        <MovieCard key={movie._id} movie={movie} onClick={movie => this.onMovieClick(movie)} />
-                    ))
-                }
+                <Row>
+                    {selectedMovie ? (
+                        <MovieView movie={selectedMovie} />
+                    ) : (
+                            movies.map((movie) => (
+                                <Col>
+                                    <MovieCard
+                                        key={movie._id}
+                                        movie={movie}
+                                        onClick={(movie) => this.onMovieClick(movie)}
+                                    />
+                                </Col>
+                            ))
+                        )}
+                </Row>
             </div>
         );
     }
